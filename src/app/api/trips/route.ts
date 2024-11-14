264cfb0ff098940ca79190687b34000f99015623
Yerikho William Tasilima <yerikhowilliamt@gmail.com>
2024-11-14 11:21:40 +0700

    fix: api bad request

--- src/app/api/trips/route.ts
+++ src/app/api/trips/route.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 export async function GET() {
   try {
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/yellow-taxi-trips`
+      `${process.env.NEXT_PUBLIC_API_URL}/api/yellow-taxi-trips?page=1&limit=10`
     );
 
     if (response.status !== 200) {
