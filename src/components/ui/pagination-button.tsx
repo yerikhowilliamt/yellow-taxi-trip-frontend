55bb3751953a8a1a8c8ec0baabb7d6a20d127c25
Yerikho William Tasilima <yerikhowilliamt@gmail.com>
2024-11-29 19:36:46 +0700

    fix: pagination button mobile display

--- src/components/ui/pagination-button.tsx
+++ src/components/ui/pagination-button.tsx
@@ -11,14 +11,14 @@ const PaginationButton: React.FC<PaginationButtonProps> = ({decrement, increment
     <div className='w-full flex justify-center gap-2 mt-4'>
         <Button
           onClick={decrement}
-          className='min-w-24 bg-white text-black hover:text-white hover:bg-[#ffa702] transition-colors duration-700 ease-in-out'
+          className='min-w-12 w-full max-w-24 bg-white text-black hover:text-white hover:bg-[#ffa702] transition-colors duration-700 ease-in-out'
         >
           Prev
         </Button>
         <span className='text-white font-semibold flex items-center justify-center px-3'>{page}</span>
         <Button
           onClick={increment}
-          className='min-w-24 bg-white text-black hover:text-white hover:bg-[#ffa702] transition-colors duration-700 ease-in-out'>
+          className='min-w-12 w-full max-w-24 bg-white text-black hover:text-white hover:bg-[#ffa702] transition-colors duration-700 ease-in-out'>
           Next
         </Button>
       </div>
